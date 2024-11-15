b1f9b64e9d8132fe7b004711cedda65d666d73a1
Eric Skaftason <skaftasoneric@gmail.com>
2024-11-15 15:20:24 -0500

    Added btn to allow page request cancellation

--- blocked/blocked.js
+++ blocked/blocked.js
@@ -1,6 +1,8 @@
 const url_params = new URLSearchParams(window.location.search);
 const og_link = url_params.get('link');
 
+let counting = false;
+
 document.addEventListener('click', (event) => {
     const target = event.target;
     switch (target.id) {
@@ -9,33 +11,32 @@ document.addEventListener('click', (event) => {
             break;
         case 'close_page':
             closePage();
-            return;
+            break;
+        case 'overlay_cancel_btn':
+            closeOverlay();
+            break;
     }
 });
 
 function returnToPage() {
     let count = 10;
 
-    const overlay = document.createElement('div');
-    const createOverlay = () => {
-        overlay.classList.add('overlay');
-        overlay.innerText = `Returning in ${count} seconds.`;
-
-        document.body.append(overlay);
-    }
-    createOverlay();
+    createOverlay(count);
 
     const countdown = () => {
-        console.log('count')
+        counting = true;
         const interval_countdown = setInterval(() => {
             if (count === 0) {
                 clearInterval(interval_countdown);
                 sendToPage();
                 return;
             }
-            console.log(count)
+            if (!counting) {
+                clearInterval(interval_countdown);
+                return;
+            }
             count--;
-            overlay.innerText = `Returning in ${count} seconds.`;
+            document.querySelector('#overlay_txt').innerText = `Returning in ${count} seconds.`;
         }, 1000); // Set interval to 1000 ms (1 second)
     };
 
@@ -50,4 +51,28 @@ function returnToPage() {
 
 function closePage() {
     window.close();
+}
+
+function createOverlay(count) {
+    const overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    
+    const overlayTxt = document.createElement('h2');
+    overlayTxt.id = 'overlay_txt';
+    overlayTxt.innerText = `Returning in ${count} seconds.`;
+
+    const cancelBtn = document.createElement('button');
+    cancelBtn.id = 'overlay_cancel_btn';
+    cancelBtn.innerText = "Cancel";
+
+    overlay.append(overlayTxt);
+    overlay.append(cancelBtn);
+
+    document.body.append(overlay);
+}
+
+function closeOverlay() {
+    counting = false;
+    const overlay = document.querySelector('.overlay');
+    overlay.remove();
 }
